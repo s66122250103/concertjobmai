c196043f3ebf19ecf1315072bbea4b6ee81d12bc
s66122250103 <s66122250103@ssru.ac.th>
2026-09-18 15:45:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'C:/xampp/htdocs/Concert/config/database.php';
+require_once(__DIR__ . '/config/database.php');
 require_once 'C:/xampp/htdocs/Concert/includes/auth.php';
 if (isLoggedIn()) {
     header('Location: http://localhost/Concert/pages/events.php');

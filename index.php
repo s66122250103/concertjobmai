47e15c763e98003cb1b446d16ac0a7cebceaa919
s66122250103 <s66122250103@ssru.ac.th>
2026-09-18 15:47:17 +0700

    Update file paths to use relative URLs

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 require_once(__DIR__ . '/config/database.php');
-require_once 'C:/xampp/htdocs/Concert/includes/auth.php';
+require_once(__DIR__ . '/includes/auth.php');
 if (isLoggedIn()) {
-    header('Location: http://localhost/Concert/pages/events.php');
+    header('Location: /pages/events.php');
 } else {
-    header('Location: http://localhost/Concert/pages/login.php');
+    header('Location: /pages/login.php');
 }
 exit;
